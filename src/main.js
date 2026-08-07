b9c569cbee3145b57713cafc534ff6277e456d21
Alexandr Madibaev <90343265+AlexMadibaev@users.noreply.github.com>
2026-08-07 13:02:44 +0500

    Set camera focal length to 20mm

--- src/main.js
+++ src/main.js
@@ -34,6 +34,7 @@ scene.background = new THREE.Color(0xa9c7d8);
 scene.fog = new THREE.FogExp2(0xa9c7d8, 0.00018);
 
 const camera = new THREE.PerspectiveCamera(70, innerWidth / innerHeight, 0.05, 100000);
+camera.setFocalLength(20);
 camera.rotation.order = 'YXZ';
 const renderer = new THREE.WebGLRenderer({ canvas, antialias: true, powerPreference: 'high-performance' });
 renderer.setPixelRatio(Math.min(devicePixelRatio, 2));
